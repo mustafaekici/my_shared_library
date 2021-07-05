1d8e022a4f2c9419afe420934dedfabc6acdf574
Ekici <muekici@emea.corpdir.net>
2021-07-05 16:37:02 +0300

    Create ci.groovy

---
+++ src/com/dai/ci.groovy
@@ -0,0 +1,21 @@
+
+#!/usr/bin/groovy
+package com.dai
+
+def PythonLinter(){
+    return true;
+}
+
+def Blackduck(){
+    return true;
+}
+
+def Documentation(){
+    return true;
+}
+
+def ArtifactoryCleanup(){
+    return true;
+}
+
+return this
