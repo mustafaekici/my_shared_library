6f24d278f8c11819a060eb5b644d0952f1351ce9
Ekici <muekici@emea.corpdir.net>
2021-07-06 10:27:18 +0300

    Update ci.groovy

--- src/com/dai/ci.groovy
+++ src/com/dai/ci.groovy
@@ -17,39 +17,15 @@ def ArtifactoryCleanup(){
 }
 
 def foo(){
-    pipeline {
-    agent none
-
-    parameters {
-        string (
-            name: 'Jira_Key',
-            defaultValue: "",
-            description: 'OPTIONAL. Add the Jira ticket to the Testrun / Real deployment. E.g ITFADAS-1501'
-        )
-
-        string (
-            name: 'API',
-            defaultValue: "",
-            description: 'OPTIONAL. Docu https://gsep.daimler.com/confluence/x/dymhGw Comma separated list of key-value pairs with double "::". E.g: Target_Env::DASK_MTC_TEST, Jira_Key::ITFADAS-1501'
-        )
-
-        choice (
-            name: "DEBUG", choices: ['', 'false', 'true'], description: "Set True to verbose the console output."
-        )
-    }
-
-    options {
-        buildDiscarder(logRotator(numToKeepStr: '20'))
-        timeout(time: 120, unit: 'MINUTES')
-        disableConcurrentBuilds()
-        timestamps()
-    }
+   pipeline {
+    agent any
     stages{
         stage("Run factory for statemachine"){
-            
+          
         }
     }
-    }
+}
+    
 }
 
 return this
