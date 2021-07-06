fd98dc76860df4c39c49ce878a6cb600063adc78
Ekici <muekici@emea.corpdir.net>
2021-07-06 11:53:29 +0300

    Update MyPipeline.groovy

--- src/com/dai/MyPipeline.groovy
+++ src/com/dai/MyPipeline.groovy
@@ -1,19 +1,19 @@
 package com.dai
-public class mypipeline{
+class MyPipeline{
 
     def steps
     
-    mypipeline(steps) {this.steps=steps}
+    MyPipeline(steps) {this.steps=steps}
     
-    public def foo(){
-        steps.pipeline {
-        steps.agent any
-        steps.stages{
-            steps.stage("Run factory for statemachine"){
+    def foo(){
+        // steps.pipeline {
+        // steps.agent any
+        // steps.stages{
+        //     steps.stage("Run factory for statemachine"){
                
-                }
-            }
-        }
+        //         }
+        //     }
+        // }
     }
     
     
